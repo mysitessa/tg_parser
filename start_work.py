65ffdd6e24147a2ad16a166b5e2f57b58709f599
gelik <136448734+mysitessa@users.noreply.github.com>
2026-01-23 14:19:21 +0300

    add dcoker

--- start_work.py
+++ start_work.py
@@ -1,20 +1,20 @@
-from decouple import config
-
+import os
 from telethon import TelegramClient, events
 
-api_id = '24376250'
+api_id = 24376250
 api_hash = '2a470c2c5891f05f67ab97c9437ca6a7'
 
 SOURCE_CHAT = -1001487687256
 TARGET_CHAT = -1003641327064
 
-client = TelegramClient("session", api_id, api_hash)
+client = TelegramClient("session.session", api_id, api_hash)
 
 @client.on(events.NewMessage(chats=SOURCE_CHAT))
 async def forward_all_messages(event):
-    await client.forward_messages(
-        entity=TARGET_CHAT,
-        messages=event.message
+    await client.send_message(
+        TARGET_CHAT,
+        event.message.text or "",
+        file=event.message.media
     )
 
 async def main():
